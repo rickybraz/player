8d1af56464bca608f9d94c2e3b7b349f89954917
Ricardo Braz <rickybraz@gmail.com>
2020-04-05 21:47:19 -0300

    Conclui os botões a inclusão das funções deles

--- _js/chamadas.js
+++ _js/chamadas.js
@@ -4,3 +4,23 @@ function busca(tecla){
     }
     
 }
+
+function musicNext(){
+    window.alert("Vai para proxima música")
+}
+
+function musicBack(){
+    window.alert("Volta para musica anterior")
+}
+
+function musicPlay(){
+    window.alert("Começa ou pausa a musica")
+}
+
+function musicVol(){
+    window.alert("Aumenta ou baixa o volume")
+}
+
+function musicBest(){
+    window.alert("Favorita a musica")
+}
