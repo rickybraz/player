711c47fb9bd4c25a403811ca1752238592ea52c4
Ricardo Braz <rickybraz@gmail.com>
2020-04-13 23:25:09 -0300

    Feito os links e algumas coisas

--- _js/chamadas.js
+++ _js/chamadas.js
@@ -24,3 +24,90 @@ function musicVol(){
 function musicBest(){
     window.alert("Favorita a musica")
 }
+
+function EstiloSel(){
+    var aux = document.getElementById("menuAlfabetico")
+    aux.innerHTML = ``
+    document.getElementById('painel').src = "_paginas/capa.html"
+}
+
+function musicTocador(codigo) {
+    window.alert("tocador")
+}
+
+function ArtistaAlfa(){
+
+    var aux = document.getElementById("menuAlfabetico")
+    aux.innerHTML = `<tr>
+    <td><a href='javascript:ArtistaSel(1)'>A</td>
+    <td><a href='javascript:ArtistaSel(2)'>B</td>
+    <td><a href='javascript:ArtistaSel(3)'>C</td>
+    <td><a href='javascript:ArtistaSel(4)'>D</td>
+    <td><a href='javascript:ArtistaSel(5)'>E</td>
+    <td><a href='javascript:ArtistaSel(6)'>F</td>
+    <td><a href='javascript:ArtistaSel(7)'>G</td>
+    <td><a href='javascript:ArtistaSel(8)'>H</td>
+    <td><a href='javascript:ArtistaSel(9)'>I</td>
+    <td><a href='javascript:ArtistaSel(10)'>J</td>
+    <td><a href='javascript:ArtistaSel(11)'>K</td>
+    <td><a href='javascript:ArtistaSel(12)'>L</td>
+    <td><a href='javascript:ArtistaSel(13)'>M</td>
+    <td><a href='javascript:ArtistaSel(14)'>N</td>
+    <td><a href='javascript:ArtistaSel(15)'>O</td>
+    <td><a href='javascript:ArtistaSel(16)'>O</td>
+    <td><a href='javascript:ArtistaSel(17)'>Q</td>
+    <td><a href='javascript:ArtistaSel(18)'>R</td>
+    <td><a href='javascript:ArtistaSel(19)'>S</td>
+    <td><a href='javascript:ArtistaSel(20)'>T</td>
+    <td><a href='javascript:ArtistaSel(21)'>U</td>
+    <td><a href='javascript:ArtistaSel(22)'>V</td>
+    <td><a href='javascript:ArtistaSel(23)'>X</td>
+    <td><a href='javascript:ArtistaSel(24)'>Y</td>
+    <td><a href='javascript:ArtistaSel(25)'>W</td>
+    <td><a href='javascript:ArtistaSel(26)'>Z</td>
+    </tr>`
+
+}
+
+function ArtistaSel(letra) { 
+    document.getElementById('painel').src = "_paginas/artista.html"
+}
+
+function AlbunsAlfa(){
+ 
+    var aux = document.getElementById("menuAlfabetico")
+    aux.innerHTML = `<tr>
+    <td><a href='javascript:AlbumSel(1)'>A</td>
+    <td><a href='javascript:AlbumSel(2)'>B</td>
+    <td><a href='javascript:AlbumSel(3)'>C</td>
+    <td><a href='javascript:AlbumSel(4)'>D</td>
+    <td><a href='javascript:AlbumSel(5)'>E</td>
+    <td><a href='javascript:AlbumSel(6)'>F</td>
+    <td><a href='javascript:AlbumSel(7)'>G</td>
+    <td><a href='javascript:AlbumSel(8)'>H</td>
+    <td><a href='javascript:AlbumSel(9)'>I</td>
+    <td><a href='javascript:AlbumSel(10)'>J</td>
+    <td><a href='javascript:AlbumSel(11)'>K</td>
+    <td><a href='javascript:AlbumSel(12)'>L</td>
+    <td><a href='javascript:AlbumSel(13)'>M</td>
+    <td><a href='javascript:AlbumSel(14)'>N</td>
+    <td><a href='javascript:AlbumSel(15)'>O</td>
+    <td><a href='javascript:AlbumSel(16)'>O</td>
+    <td><a href='javascript:AlbumSel(17)'>Q</td>
+    <td><a href='javascript:AlbumSel(18)'>R</td>
+    <td><a href='javascript:AlbumSel(19)'>S</td>
+    <td><a href='javascript:AlbumSel(20)'>T</td>
+    <td><a href='javascript:AlbumSel(21)'>U</td>
+    <td><a href='javascript:AlbumSel(22)'>V</td>
+    <td><a href='javascript:AlbumSel(23)'>X</td>
+    <td><a href='javascript:AlbumSel(24)'>Y</td>
+    <td><a href='javascript:AlbumSel(25)'>W</td>
+    <td><a href='javascript:AlbumSel(26)'>Z</td>
+    </tr>`
+
+}
+
+function AlbumSel(letra) {
+    document.getElementById('painel').src = "_paginas/album.html"
+}
+
